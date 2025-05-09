a9b6ceedf83fe17ed6169e2be2830a68cce5a8e3
hebert <hebertsantossoaress@gmail.com>
2025-05-09 19:51:12 -0300

    feat: Lista 5

--- LP/Listas de Exercícios/LP_Lista_05/exercicio_12/src/Main.java
+++ LP/Listas de Exercícios/LP_Lista_05/exercicio_12/src/Main.java
@@ -7,8 +7,7 @@ public class Main {
                 {7, 8, 9}
         };
 
-        // Calcula o determinante pela Regra de Sarrus
-        int determinante =
+    
                 matriz[0][0] * matriz[1][1] * matriz[2][2]
                         + matriz[0][1] * matriz[1][2] * matriz[2][0]
                         + matriz[0][2] * matriz[1][0] * matriz[2][1]
@@ -16,7 +15,7 @@ public class Main {
                         - matriz[0][0] * matriz[1][2] * matriz[2][1]
                         - matriz[0][1] * matriz[1][0] * matriz[2][2];
 
-        // Exibe a matriz e o determinante
+        
         System.out.println("Matriz 3x3:");
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
